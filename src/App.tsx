16eaad79ab63d628c1a93d64f1f4b2491dd55886
pphelpme <98599731+pphelpme@users.noreply.github.com>
2022-06-26 18:13:22 +0800

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='w-full h-full bg-slate-800'>test</div>
+    </div>
+  );
+}
+
+export default App;
